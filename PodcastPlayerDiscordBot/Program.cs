072f75ca7460df708b5835d03616201fdae68861
Alexander Lindsay <aml.lindsay@gmail.com>
2016-06-25 09:53:14 -0700

    created inital outline of NAudio integration
    - used the read fully stream from the NAudio streaming sample
    - actually lots of code from that sample
    - changed to skip frames that don't match
    - changed to not write directly to buffered wave to allow it to later send the data to discord instead of an NAudio WaveOut
    - set up a test that uses WaveOut to make sure it works for now
    - test uses the black tapes as an audio source :p

--- PodcastPlayerDiscordBot/Program.cs
+++ PodcastPlayerDiscordBot/Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using NAudio.Wave;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace PodcastPlayerDiscordBot
@@ -10,6 +12,47 @@ namespace PodcastPlayerDiscordBot
     {
         static void Main(string[] args)
         {
+            var url = "http://traffic.libsyn.com/theblacktapes/THE_BLACK_TAPES_EPISODE_207_-_Personal_Possessions.mp3";
+            var speaker = new Speaker(url);
+
+            BufferedWaveProvider bufferedWaveProvider = null;
+
+            ThreadPool.QueueUserWorkItem(delegate
+            {
+                speaker.Play(
+                    () =>
+                    {
+                        return bufferedWaveProvider != null &&
+                           bufferedWaveProvider.BufferLength - bufferedWaveProvider.BufferedBytes
+                           < bufferedWaveProvider.WaveFormat.AverageBytesPerSecond / 4;
+                    },
+                    (b, offset, count, format) =>
+                    {
+                        if (bufferedWaveProvider == null)
+                        {
+                            bufferedWaveProvider = new BufferedWaveProvider(format);
+                            bufferedWaveProvider.BufferDuration = TimeSpan.FromSeconds(20);
+                        }
+                        bufferedWaveProvider.AddSamples(b, offset, count);
+                    },
+                    (message) =>
+                    {
+                        Console.WriteLine(message);
+                    });
+            });
+
+            Console.ReadLine();
+
+            using (var waveOut = new WaveOut(WaveCallbackInfo.FunctionCallback()))
+            {
+                var volumeProvider = new VolumeWaveProvider16(bufferedWaveProvider);
+                waveOut.Init(volumeProvider);
+                waveOut.Play();
+                while (waveOut.PlaybackState == PlaybackState.Playing)
+                {
+                    Thread.Sleep(100);
+                }
+            }
         }
     }
 }
---
+++ PodcastPlayerDiscordBot/Speaker.cs
@@ -0,0 +1,146 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PodcastPlayerDiscordBot
+{
+    public class Speaker
+    {
+        private object playerLock = new object();
+
+        private Uri source { get; set; }
+        private bool IsPlaying { get; set; }
+
+        public Speaker(string url) : this(new Uri(url)) { }
+
+        public Speaker(Uri url)
+        {
+            source = url;
+        }
+
+        public void Stop()
+        {
+            lock (playerLock)
+            {
+                IsPlaying = false;
+            }
+        }
+
+        public void Play(Func<bool> shouldPauseBuffering, Action<byte[], int, int, WaveFormat> addToBuffer, Action<string> reportError)
+        {
+            var webRequest = (HttpWebRequest)WebRequest.Create(source);
+            HttpWebResponse resp;
+            try
+            {
+                resp = (HttpWebResponse)webRequest.GetResponse();
+            }
+            catch (WebException e)
+            {
+                if (e.Status != WebExceptionStatus.RequestCanceled)
+                {
+                    reportError(e.Message);
+                }
+                return;
+            }
+            var buffer = new byte[16384 * 4];
+
+            lock (playerLock)
+            {
+                IsPlaying = true;
+            }
+
+            IMp3FrameDecompressor decompressor = null;
+            try
+            {
+                using (var responseStream = resp.GetResponseStream())
+                {
+                    var readFullyStream = new ReadFullyStream(responseStream);
+
+                    Mp3Frame frame;
+                    List<Mp3Frame> frames = Enumerable.Range(0, 10).Select(i => Mp3Frame.LoadFromStream(readFullyStream)).ToList();
+
+                    while (frames.Select(f => new { SampleRate = f.SampleRate, ChannelMode = f.ChannelMode }).Distinct().Count() != 1)
+                    {
+                        frames.RemoveAt(0);
+                        frames.Add(Mp3Frame.LoadFromStream(readFullyStream));
+                    }
+
+                    bool keepPlaying;
+
+                    do
+                    {
+                        if (shouldPauseBuffering())
+                        {
+                            Thread.Sleep(500);
+                        }
+                        else
+                        {
+                            try
+                            {
+                                if (frames.Any())
+                                {
+                                    frame = frames.First();
+                                    frames.RemoveAt(0);
+                                }
+                                else
+                                {
+                                    frame = Mp3Frame.LoadFromStream(readFullyStream);
+                                }
+                            }
+                            catch (EndOfStreamException)
+                            {
+                                lock (playerLock)
+                                {
+                                    IsPlaying = false;
+                                }
+                                // reached the end of the MP3 file / stream
+                                break;
+                            }
+                            catch (WebException)
+                            {
+                                // probably we have aborted download from the GUI thread
+                                break;
+                            }
+                            if (decompressor == null)
+                            {
+                                decompressor = CreateFrameDecompressor(frame);
+                            }
+                            int decompressed = decompressor.DecompressFrame(frame, buffer, 0);
+                            addToBuffer(buffer, 0, decompressed, decompressor.OutputFormat);
+                        }
+
+                        lock (playerLock)
+                        {
+                            keepPlaying = IsPlaying;
+                        }
+
+                    } while (keepPlaying);
+                    // was doing this in a finally block, but for some reason
+                    // we are hanging on response stream .Dispose so never get there
+                    decompressor.Dispose();
+                }
+            }
+            finally
+            {
+                if (decompressor != null)
+                {
+                    decompressor.Dispose();
+                }
+            }
+        }
+
+        private static IMp3FrameDecompressor CreateFrameDecompressor(Mp3Frame frame)
+        {
+            WaveFormat waveFormat = new Mp3WaveFormat(frame.SampleRate, frame.ChannelMode == ChannelMode.Mono ? 1 : 2,
+                frame.FrameLength, frame.BitRate);
+            return new AcmMp3FrameDecompressor(waveFormat);
+
+        }
+    }
+}
